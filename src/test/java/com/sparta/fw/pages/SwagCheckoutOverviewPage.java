8f64de50b1bb97309a7ff71130086c9ab8441611
farrah <farrahwaraich@hotmail.com>
2022-07-07 14:40:27 +0100

    Added links to the burger menu and to cart page

--- src/test/java/com/sparta/fw/pages/SwagCheckoutOverviewPage.java
+++ src/test/java/com/sparta/fw/pages/SwagCheckoutOverviewPage.java
@@ -2,6 +2,10 @@ package com.sparta.fw.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class SwagCheckoutOverviewPage {
 
@@ -13,6 +17,8 @@ public class SwagCheckoutOverviewPage {
     private By total = new By.ByClassName("summary_total_label");
     private By tax = new By.ByClassName("summary_tax_label");
     private By totalBeforeTax = new By.ByClassName("summary_subtotal_label");
+    private By burgerMenuLink= new By.ById("react-burger-menu-btn");
+    private By cartLink= new By.ByClassName("shopping_cart_link");
 
     public SwagCheckoutOverviewPage(WebDriver driver){
         this.driver = driver;
@@ -21,7 +27,37 @@ public class SwagCheckoutOverviewPage {
     public String getURL() {
         return driver.getCurrentUrl();
     }
+    public SwagCheckoutFinishPage goToCheckoutFinishPage(){
+        driver.findElement(finish).click();
+        return new SwagCheckoutFinishPage(driver);
+    }
 
+    public SwagLoginPage goToLoginPage(){
+        driver.findElement(burgerMenuLink).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("logout_sidebar_link")));
+        driver.findElement(By.id("logout_sidebar_link")).click();
+        return new SwagLoginPage(driver);
+    }
+    public WebDriver goToAboutPage(){
+        driver.findElement(burgerMenuLink).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("about_sidebar_link")));
+        driver.findElement(By.id("about_sidebar_link")).click();
+        return driver;
+    }
+    public WebDriver goToResetAppState(){
+        driver.findElement(burgerMenuLink).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("reset_sidebar_link")));
+        driver.findElement(By.id("reset_sidebar_link")).click();
+        return driver;
+    }
+
+    public SwagCartPage goToCartPage(){
+        driver.findElement(cartLink).click();
+        return new SwagCartPage(driver);
+    }
     public boolean goToFinish(){
         driver.findElement(finish).click();
         return getURL().equals("https://www.saucedemo.com/checkout-complete.html");
@@ -37,10 +73,10 @@ public class SwagCheckoutOverviewPage {
         return driver;
     }
 
-    public boolean taxCalculation(){
-        double taxPercentage = 1.08;
-        driver.findElement(tax).equals(totalBeforeTax * taxPercentage);
-
-    }
+//    public boolean taxCalculation(){
+//        double taxPercentage = 1.08;
+//        driver.findElement(tax).getText().equals(String.valueOf(totalBeforeTax * taxPercentage));
+//
+//    }
 
 }
