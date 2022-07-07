78378911d4a0c5698005c55fc636459cbf275fc3
farrah <farrahwaraich@hotmail.com>
2022-07-07 12:30:59 +0100

    updates on overview page

--- src/test/java/com/sparta/fw/pages/SwagCheckoutOverviewPage.java
+++ src/test/java/com/sparta/fw/pages/SwagCheckoutOverviewPage.java
@@ -9,6 +9,10 @@ public class SwagCheckoutOverviewPage {
 
     private By finish = new By.ById("finish");
     private By cancel = new By.ById("cancel");
+    private By quantity = new By.ByClassName("cart_quantity");
+    private By total = new By.ByClassName("summary_total_label");
+    private By tax = new By.ByClassName("summary_tax_label");
+    private By totalBeforeTax = new By.ByClassName("summary_subtotal_label");
 
     public SwagCheckoutOverviewPage(WebDriver driver){
         this.driver = driver;
@@ -18,15 +22,29 @@ public class SwagCheckoutOverviewPage {
         return driver.getCurrentUrl();
     }
 
-    public WebDriver goToFinish(){
+    public boolean goToFinish(){
         driver.findElement(finish).click();
+        return getURL().equals("https://www.saucedemo.com/checkout-complete.html");
+    }
+
+    public boolean goToCancel(){
+        driver.findElement(cancel).click();
+        return getURL().equals("https://www.saucedemo.com/inventory.html");
+    }
+
+    public WebDriver changeQuantityOnOverviewPage(){
+        driver.findElement(quantity).sendKeys("2");
         return driver;
     }
 
-    public WebDriver goToCancel(){
-        driver.findElement(cancel).click();
-        return driver;
+    public boolean taxCalculation(){
+        double taxPercentage = 1.08;
+        driver.findElement(tax).equals(totalBeforeTax * taxPercentage);
+
     }
+
+
+
 
 
 }
